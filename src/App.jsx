7f5e05dd96bd6fbb3673f2f0c49697fa06eabbae
Drach09 <dangerdagger123456@gmail.com>
2026-04-25 13:36:17 +0530

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   // Load the local static site directly (no iframe).
   if (typeof window !== 'undefined') {
-    window.location.replace('/site/mastersunion.org/index.html')
+    window.location.replace('/site/index.html')
   }
   return null
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// If we’re already on the static site, don’t boot React.
-if (window.location.pathname.startsWith('/site/mastersunion.org/')) {
+// If we're already on the static site, don't boot React.
+if (window.location.pathname.startsWith('/site/')) {
   // The static HTML page will take over.
 } else {
 createRoot(document.getElementById('root')).render(
